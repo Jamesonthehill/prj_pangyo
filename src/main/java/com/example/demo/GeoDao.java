7ade63dedf47f54d85d6a209e5ac55fdbd995e27
James <dlrjsdn53@naver.com>
2022-09-08 23:55:12 +0900

    GeoDao, GeoMapper.xml, GeoService.java

---
+++ src/main/java/com/example/demo/GeoDao.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import java.util.HashMap;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class GeoDao{  
+	
+	
+	@Inject
+//	@Resource(name = "sqlSession")
+	private SqlSession sqlSession; // sqlSession의 사용을 정의 SQL에서 db를 받기위한 논리적연결상태를 말함.
+	
+	private static String namespace="com.example.demo.GeoMapper";
+	
+	public List<HashMap<String, Object>> getSelectList(){ 
+		
+		System.out.println("HashMap : " + sqlSession.selectList(namespace + ".getSelectList").toString()); 
+		
+		return sqlSession.selectList(namespace + ".getSelectList" ); }
+}
--- src/main/java/com/example/demo/GeoService.java
+++ src/main/java/com/example/demo/GeoService.java
@@ -1,7 +1,25 @@
 package com.example.demo;
 
+import java.util.HashMap;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+//@RequiredArgsConstructor
+@Service ("GeoService")
+public class GeoService {
 
-
-//public List<String, Object>
+	@Autowired
+	GeoDao dao;
+	
+//	
+//	public List<Test> getSelectList() throws Exception {
+//		
+//		return dao.getSelectList();
+//	}
+	
+	public List<HashMap<String, Object>> getSelectList() {
+		return dao.getSelectList();
+	}
+}
